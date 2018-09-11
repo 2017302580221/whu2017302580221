f4724bee178b302a8eaa13270f5e088253bb4f42
WY <2589983357@qq.com>
2018-09-12 00:04:12 +0800

    first homework

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Homework1
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double m = 0;
+            double n = 0;
+            Console.WriteLine("Please input a number:");
+            s = Console.ReadLine();
+            m = double.Parse(s);
+            Console.WriteLine("Please input a number:");
+            s = Console.ReadLine();
+            n = Double.Parse(s);
+            double p = m * n;
+            Console.WriteLine(m + "*" + n + "=" + p);
+        }
+    }
+}
---
+++ homework2/homework2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            double d1 = double.Parse(s1);
+            string s2 = textBox2.Text;
+            double d2 = double.Parse(s2);
+            double d3 = d1 * d2;
+            label1.Text = d1 + "*" + d2 + "=" + d3;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
